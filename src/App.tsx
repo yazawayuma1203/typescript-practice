82b5511060629507c14509f966584291ac6d22ca
yazawayuma1203 <110238678+yazawayuma1203@users.noreply.github.com>
2022-08-14 12:17:32 +0900

    型がなくバグ発生 型を指定せずにバグ発生

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
